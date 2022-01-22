05f18f24e46cf405e3a84b1bdf60065b0dbd27ad
BrunoCarvalho18 <brunocarvalhodesa@gmail.com>
2022-01-22 17:39:58 -0300

    first commit

---
+++ cypress/integration/api-tests/book.spec.js
@@ -0,0 +1,26 @@
+const { it } = require("mocha");
+
+describe("Search a book", () => {
+  let id = 1;
+
+  it("search a book - GET", () => {
+    cy.request("GET", `/posts/${id}`).as("response");
+    cy.get("@response").then((res) => {
+      console.log("response", res);
+      expect(res.status).to.be.equal(200);
+    });
+  });
+
+  it("create a book - POST", () => {
+    cy.request("POST", "/posts", {
+      userId: 1,
+      id: 1,
+      title: "teste",
+      body: "teste",
+    }).as("response");
+    cy.get("@response").then((res) => {
+      console.log("response", res);
+      expect(res.status).to.be.equal(201);
+    });
+  });
+});
